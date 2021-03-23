4b42d99cd5c1e080bf76a3a1ede336e53529b904
TheTeslablyatster <58738428+TheTeslablyatster@users.noreply.github.com>
2021-03-23 03:13:13 +0200

    Добавьте файлы проекта.

---
+++ RugbyScore/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace RugbyScore
+{
+    class Program
+    {
+        static void Main()
+        {
+            int Score = int.Parse(Console.ReadLine());
+            for (int i = 0; i <= Score / 5; i++)
+                for (int j = 0; j <= i; j++)
+                    if (((Score - 5 * i - 2 * j) % 3 == 0) && (Score - 5 * i - 2 * j) / 3 >= 0)
+                        Console.WriteLine($"{i} {j} {(Score - 5 * i - 2 * j) / 3}");
+            Console.ReadLine();
+        }
+    }
+}
